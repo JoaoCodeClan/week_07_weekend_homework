5528bff4ad39b3ad84c1dd684173f02e18d60856
Joao <johnynecas@hotmail.com>
2017-11-10 11:36:56 +0000

    added generic class enclosures first getters and first zoo class getters

--- Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/Zoo.java
+++ Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/Zoo.java
@@ -1,8 +1,29 @@
 package codeclan.com.joaozoo;
 
+import java.util.ArrayList;
+
+import codeclan.com.joaozoo.models.Enclosures;
+
 /**
  * Created by user on 10/11/2017.
  */
 
 public class Zoo {
+    private String name;
+    private ArrayList<Enclosures> enclosures;
+
+    public Zoo(String name){
+        this.name = name;
+        this.enclosures= new ArrayList();
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name= newName;
+
+    }
 }
--- Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/models/Enclosures.java
+++ Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/models/Enclosures.java
@@ -7,11 +7,31 @@ import java.util.ArrayList;
  */
 
 public class Enclosures <T> {
+    private String nameOfEnclosure;
     private ArrayList<T>animals;
 
-    public Enclosures(){
+    public Enclosures(String nameOfEnclosure){
+        this.nameOfEnclosure = nameOfEnclosure;
         this.animals = new ArrayList();
     }
 
-    
+ public String getNameOfEnclosure(){
+     return this.nameOfEnclosure;
+ }
+ public void setNameOfEnclosure(String newName){
+     this.nameOfEnclosure = newName;
+
+ }
+
+ public int getNumberOfAnimalsInEnclosure(){
+     return this.animals.size();
+ }
+
+ public void addAnimalToEnclosure(T animal){
+     this.animals.add(animal);
+ }
+
+ public void removeAnimalFromEnclosure(T animal){
+        this.animals.add(animal);
+    }
 }
