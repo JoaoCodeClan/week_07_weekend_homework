fd083220fdcf51c96674d7b26fb5c5abc5d99916
Joao <johnynecas@hotmail.com>
2017-11-10 11:05:41 +0000

    created Animals superclass and lions and elephants subclasses

--- Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/models/Elephants.java
+++ Zoo/MyApplication/app/src/main/java/codeclan/com/joaozoo/models/Elephants.java
@@ -4,5 +4,11 @@ package codeclan.com.joaozoo.models;
  * Created by user on 10/11/2017.
  */
 
-public class Elephants {
+public class Elephants extends Animals {
+    String name;
+
+    public Elephants(String name, int price){
+        super(price);
+        this.name = name;
+    }
 }
